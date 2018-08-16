ff953613faf5abbf87f11f24e15785764ae823e7
fenghaikuan <fenghaikuan@xiangshang360.com>
2018-08-16 15:47:36 +0800

    redis mongo

--- mongodb/movedata.sh
+++ mongodb/movedata.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 ###迁移mongodb数据###
 ##迁移前的信息##
-localHost=""
-localPort=""
+localHost="127.0.0.1"
+localPort="27017"
 localDatabase=""
 localAuthpasswd=""
 ##迁移后的信息##
 descHost=""
-descPort=""
+descPort="27017"
 descDatabase=""
 descAuthpasswd=""
 ##需要迁移的collection##
--- redis/movedata.sh
+++ redis/movedata.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 ###迁移redis数据###
+##迁移前的信息##
+localHost="127.0.0.1"
+localPort="6379"
+localAuthpasswd=""
+##迁移后的信息##
+descHost=""
+descPort=""
+descAuthpasswd=""
 
+dataDir="./redisdata"
 #1、导出数据
 #2、导入数据
