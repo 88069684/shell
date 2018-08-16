0dad225c0b367db0f9087e5698534587e86697b2
fenghaikuan <fenghaikuan@xiangshang360.com>
2018-08-16 15:45:17 +0800

    mongo

--- mongodb/movedata.sh
+++ mongodb/movedata.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 ###迁移mongodb数据###
+##迁移前的信息##
 localHost=""
 localPort=""
 localDatabase=""
 localAuthpasswd=""
-
+##迁移后的信息##
 descHost=""
 descPort=""
 descDatabase=""
 descAuthpasswd=""
-
+##需要迁移的collection##
 collections=("user" )
 dataDir="./mongodata"
-
-
 #1、导出数据
+
 #2、导入数据
